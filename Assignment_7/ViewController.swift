940aee6c48076416d31014fc4076ac8835828240
topuchi13 <nicolas1213@gmail.com>
2021-09-27 21:32:39 +0400

    Initial Commit for Assignment 7.

--- Assignment_7/ViewController.swift
+++ Assignment_7/ViewController.swift
@@ -5,9 +5,23 @@
 //  Created by Nika Topuria on 27.09.21.
 //
 
+//შექმენით აპლიკაცია, სადაც მთავარ გვერდზე დაამატებთ 4 ღილაკს, რომლებსაც ექნებათ განსხვავებული ფერები
+//ყველა ღილაკი ეკრანზე დაიკავებს თანაბარ ადგილს
+//Portrait და Landscape-ზეც ღილაკებს უნდა ჰქონდეთ თანაბარი ზომები
+//ღილაკზე დაჭერისას იხსნება სხვა გვერდი, რომელზეც ტექსტი არის ეკრანის შუაში, Portrait-ზეც და Landscape-ზეც. (სიმულატორის Landscape-ზე გადასაყვანად შეგიძლიათ გამოიყენოთ cmd + მარცხენა ან მარჯვენა ისარი)
+//ექსტრა 20 ქულა - რომელიმე ღილაკზე დაჭერისას, ახალ გახსნილი გვერდის ბექგრაუნდის ფერიც უნდა იყოს იგივე რა ფერის ღილაკსაც დააჭირა.
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let a = sender as? UIButton {
+            segue.destination.view.backgroundColor = a.backgroundColor
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
